0d13984adecdda537fda80f05ec828425e22ab69
sravanod <sravan125.vtalent@gmail.com>
2019-08-28 15:05:05 +0530

    VSTtestRepo Added to online

---
+++ VSTRepo1/VSTRepo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSTRepo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This my 1St Visual studio task
+        }
+    }
+}
